c4dae52e1ebb3b68bf5ea850070c5970502b1bc0
MiniJ147 <47308554+MiniJ147@users.noreply.github.com>
2024-01-14 19:59:44 -0500

    Basic http server w/ routing and json

---
+++ json.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Printf("Responding with 500 level error: %v", msg)
+	}
+
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+
+	/* How it looks
+	{
+	 	"error": "Something went wrong"
+	}
+	*/
+
+	respondWithJSON(w, code, errResponse{
+		Error: msg,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Failed to marshal JSON response %v", payload)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Add("Content", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
